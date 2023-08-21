83def44a5c065435bdca2d03f9cb3f376dea1b42
Thomas Waite <waite116@gmail.com>
2023-08-21 09:52:11 -0400

    new adds

--- SingleNoise_multirunner.py
+++ SingleNoise_multirunner.py
@@ -7,20 +7,29 @@ import numpy as np
 from decimal import *
 from functools import partial
 def main():
-    xml_path = 'MCCB_600.xml'
+    xml_path = 'SingleNoiseModel.xml'
     verisig_path = '../verisig'
     flowstar_path = '../flowstar/flowstar'
     dnn1_yaml = 'networks/Generator2x50_100_Decay1TestLoss1.035e-06.yml'
-    dnn2_yaml = 'networks/Contraster1x50_Decay5TestLoss47.74.yml'
-    dnn3_yaml = 'networks/Blurer1x50_Decay10TestLoss47.28.yml'
-    dnn4_yaml = 'networks/CompositeRegressor3x20_20_20_Decay0.001Loss5.15e-06Composite.yml'
-    dnn5_yaml = 'networks/ControllerSigmoid16x16.yml'
+    dnn3_yaml = 'networks/CompositeRegressor3x20_20_20_Decay0.001TestLoss5.15e-06Composite.yml'
+    dnn4_yaml = 'networks/ControllerSigmoid16x16.yml'
     args = sys.argv[1:]
     
     int_ind = args.index('-i')
     start_ind = args.index('-s')
     n_ind = args.index('-n')
-    
+    noise_type_ind = args.index('-t')
+    noise_type_str = args[noise_type_ind + 1].lower()
+    if noise_type_str == 'blur':
+        dnn2_yaml = 'networks/Blurer1x50_Decay10TestLoss47.28.yml'
+        model_yaml = 'SingleNoiseBlur_multi.yml'
+    elif noise_type_str == 'contrast':
+        dnn2_yaml = 'networks/Contraster1x50_Decay5TestLoss47.74.yml'
+        model_yaml = 'SingleNoiseContrast_multi.yml'
+    else:
+        print('Specify noise type with a -t parameter, e.g. -t blur')
+        return
+
     start_str = args[start_ind+1]
     start_val = Decimal(start_str)
     print(start_val)
@@ -35,7 +44,7 @@ def main():
     
     end_str = str(end_val)
     
-    output_path = 'output_MCCB_'+int_str+'_' + start_str + '_' + end_str
+    output_path = 'output_single_noise_'+ noise_type_str+ '_' +int_str+'_' + start_str + '_' + end_str
 
     if not os.path.exists(output_path):
         os.mkdir(output_path)
@@ -54,14 +63,14 @@ def main():
         return -1
     else:
         print("Building the base model...")
-        subprocess.run([verisig_path, '-vc=MCCB_multi.yml', '-o' ,'-nf', xml_path, dnn1_yaml, dnn2_yaml, dnn3_yaml, dnn4_yaml, dnn5_yaml])
+        subprocess.run([verisig_path, '-vc='+model_yaml, '-o' ,'-nf', xml_path, dnn1_yaml, dnn2_yaml, dnn3_yaml, dnn4_yaml])
 
-        with open('MCCB_600.model', 'r') as f:
+        with open('SingleNoiseModel.model', 'r') as f:
             in_model = f.read()
         print("Starting parallel verification")
         num_parallel = n_ints
         with multiprocessing.Pool(processes=num_parallel) as pool:
-            pool.map(partial(evaluate_conditions, legend=legend, model=in_model, output_path=output_path, flowstar_path=flowstar_path, dnn1_yaml=dnn1_yaml, dnn2_yaml=dnn2_yaml, dnn3_yaml=dnn3_yaml, dnn4_yaml=dnn4_yaml, dnn5_yaml=dnn5_yaml), test_set)
+            pool.map(partial(evaluate_conditions, legend=legend, model=in_model, output_path=output_path, flowstar_path=flowstar_path, dnn1_yaml=dnn1_yaml, dnn2_yaml=dnn2_yaml, dnn3_yaml=dnn3_yaml, dnn4_yaml=dnn4_yaml), test_set)
         return 0
 
 
@@ -70,13 +79,13 @@ def main():
 # Begin Parallel Function
 #==========================================================================================
 
-def evaluate_conditions(conditions, legend, model, output_path, flowstar_path, dnn1_yaml, dnn2_yaml, dnn3_yaml, dnn4_yaml, dnn5_yaml):
+def evaluate_conditions(conditions, legend, model, output_path, flowstar_path, dnn1_yaml, dnn2_yaml, dnn3_yaml, dnn4_yaml):
     test_model = model
     for i in range(len(legend)):
         test_model = test_model.replace(legend[i], str(conditions[i]))
 
-    with open(output_path + '/MCC_' + str(conditions[0]) + ', ' + str(conditions[1]) + '.txt', 'w') as f:
-        subprocess.run(flowstar_path + ' ' + dnn1_yaml + ' ' + dnn2_yaml + ' ' + dnn3_yaml + ' ' + dnn4_yaml + ' ' + dnn5_yaml, input=test_model, shell=True, universal_newlines=True, stdout=f)
+    with open(output_path + '/' + str(conditions[0]) + ', ' + str(conditions[1]) + '.txt', 'w') as f:
+        subprocess.run(flowstar_path + ' ' + dnn1_yaml + ' ' + dnn2_yaml + ' ' + dnn3_yaml + ' ' + dnn4_yaml, input=test_model, shell=True, universal_newlines=True, stdout=f)
     print('Finished Interval: [' + str(conditions[0]) + ', ' + str(conditions[1]) + ']')
 #===========================================================================================
 # End Parallel Function
